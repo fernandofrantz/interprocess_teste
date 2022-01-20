f18e0c0d0e9e118a7496cbfe2af0bfbafc8ecd60
fernando tonezer frantz <fernandofrantz12@gmail.com>
2022-01-20 11:58:15 -0300

    feature: homepage done

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,45 +1,69 @@
+import { Button, Tooltip } from "@mui/material"
 import { usePacient } from "../../providers/pacients"
+import { StyledHome } from "./styles"
+
+
+import { PieChart, Pie, Cell, ResponsiveContainer } from 'recharts';
+import { useNavigate } from "react-router-dom";
+
 
 export const Home = () => {
 
     const { pacient } = usePacient()
 
-    console.log(pacient)
-
     let totalPacients = pacient.length
     let activePacients = 0
-
     for (let counter = 0; counter < pacient.length; counter++) {
-        if(pacient.status === 'Ativo') {
+        if(pacient[counter].status === 'Ativo') {
             activePacients++
         }
     }
 
 
+    const navigate = useNavigate()
+    const sendTo = (path) => {
+        navigate(path)
+    }
+
+
+
     return (
         <>
-            <div>
+            <StyledHome>
                 <h2>
                    Bem vindo(a) a ACME
                 </h2>
-                <div>
-                    <table>
-                        <tr>
-                            <td>Cadastros</td>
-                            <td>Ativos</td>
-                            <td>Inativos</td>
-                        </tr>
-                        <tr>
-                            <td>{totalPacients}</td>
-                            <td>{activePacients}</td>
-                            <td>{totalPacients - activePacients}</td>
-                        </tr>
-                    </table>
+                <div className="tableDiv">
+                        <h3>Cadastros</h3>
+                        <ul className="tableHeader">
+                            <li>Totais</li>
+                            <li>Inativos</li>
+                            <li>Ativos</li>
+                        </ul>
+                        <ul className="tableContent">
+                            <li>{totalPacients}</li>
+                            <li>{totalPacients - activePacients}</li>
+                            <li>{activePacients}</li>
+                        </ul>
                 </div>
                 <h4>
                     O que desejas fazer?
                 </h4>
-            </div>
+                <Button
+                    onClick={() => sendTo('/registerPacient')}
+                    style={{margin: '0px 0px 10px 0px'}}
+                    variant="outlined"
+                    color="secondary"
+                    >Cadastrar novo paciente
+                </Button>
+                <Button
+                    onClick={() => sendTo('/consultPacient')}
+                    style={{margin: '15px 0px 0px 0px'}}
+                    variant="outlined"
+                    color="secondary"
+                    >Consultar pacientes
+                </Button>
+            </StyledHome>
         </>
     )
 }
---
+++ src/pages/home/styles.js
@@ -0,0 +1,26 @@
+import styled from "styled-components";
+
+export const StyledHome = styled.div`
+    display: flex;    
+    flex-direction: column;
+    justify-content: center;
+
+    
+    
+
+    .tableDiv {
+        .tableContent ,.tableHeader {
+            list-style: none;
+            margin: 0px;
+            padding: 0px;
+            display: flex;
+            justify-content: space-around;
+        }
+        
+
+
+
+    }
+    }
+
+`
